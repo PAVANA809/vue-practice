e04dedeee64c3ba1f98e9ba85a398d0b80996367
PAVANA809 <pavanhapavan@gmail.com>
2023-10-15 19:19:34 +0530

    initial

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,91 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <p>Locale</p>
+    <select @change="localeChange" v-model="locale">
+    <option value="US">US</option>
+    <option value="DE">DE</option>
+  </select>
   </div>
+
+  <div>
+    <p>Integer Format</p>
+    <select v-model="IntegerFormat">
+    <option value="1">#0</option>
+    <option v-if="locale == 'US'" value="2">#,###</option>
+    <option v-if="locale == 'DE'" value="3">#.###</option>
+    </select>
+  </div>
+
+  <div>
+    <p>Float Format</p>
+    <select v-model="FloatFormat">
+    <option v-if="locale == 'US'" value="US">#,###.00</option>
+    <option v-if="locale == 'DE'" value="DE">#.###,00</option>
+  </select>
+  </div>
+
+  <div v-if="locale != ''">
+    <p>Thousand seperator: {{ formattedNumber }}</p>
+    <p>Decimal seperator: {{ deciamlNumber }}</p>
+  </div>
+
 </template>
 
 <script>
-export default {
+  export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      locale: "US",
+      IntegerFormat: "1",
+      FloatFormat: "US",
+      number: 123456789,
+      deciaml: 356.67,
+    }
+  },
+  methods: {
+    localeChange() {
+      if (this.locale == 'US') {
+        this.FloatFormat = "US";
+      } else if (this.locale == 'DE'){
+        this.FloatFormat = "DE";
+      }
+    }
+  },
+  computed: {
+    formattedNumber() {
+      if (this.locale == 'US' && this.IntegerFormat != '1') {
+        return this.number.toLocaleString('en-US')
+      } else if (this.locale == 'DE' && this.IntegerFormat != '1') {
+        return this.number.toLocaleString('de-DE')
+      } else {
+        return this.number
+      }
+    },
+    deciamlNumber() {
+      if (this.locale == 'US') {
+        return this.deciaml.toLocaleString('en-US')
+      } else if (this.locale == 'DE') {
+        return this.deciaml.toLocaleString('de-DE')
+      } else {
+        return this.deciaml
+      }
+    }
   }
-}
+  }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  div{
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    margin: 10px;
+  }
+  p{
+    margin: 0 10px;
+  }
+  select{
+    margin: 0 10px;
+  }
 </style>
